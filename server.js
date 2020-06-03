ce4e0c1f42f52f147d18c66890b730f0c148d7bf
AaronW <w453130895@gmail.com>
2020-06-03 00:20:49 -0700

    extract data from database for rendering chart

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const knex = require('knex');
-const cors = require('cors');
+const express = require("express");
+const knex = require("knex");
+const cors = require("cors");
 
 const db = knex({
   client: "pg",
@@ -16,26 +16,58 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res)=>{
-  res.send('this is working')
-})
+app.get("/", (req, res) => {
+  res.send("this is working");
+});
 
-app.post('/connectivity', (req, res)=>{
-  const searchid = req.body.searchid
-  db.select('*').from('connectedmacid').where('mainid', '=', searchid)
-    .then(data =>{
+app.post("/connectivity", (req, res) => {
+  const searchid = req.body.searchid;
+  db.select("*")
+    .from("connectedmacid")
+    .where("mainid", "=", searchid)
+    .then(data => {
       if (data.length > 0) {
-        db.select('relatedmacid', 'connectivity').from('connectedmacid').where('mainid', '=', searchid)
-          .then(data =>{
-            res.json(data)
-          })
+        db.select("relatedmacid", "connectivity")
+          .from("connectedmacid")
+          .where("mainid", "=", searchid)
+          .then(data => {
+            res.json(data);
+          });
       } else {
-        res.status(400).json('id not found')
+        res.status(400).json("id not found");
       }
     })
-    .catch(console.log)
-})
+    .catch(console.log);
+});
 
-app.listen(process.env.PORT || 3001, ()=> {
-  console.log('server is running on port 3001')
-})
+app.post("/chart", (req, res) => {
+  const groupnum = req.body.groupnum;
+  let level_data = {
+    level1: [],
+    level2: [],
+    level3: [],
+    level4: []
+  };
+  db.select("*")
+    .from("devices")
+    .where("groupnum", "=", groupnum)
+    .orderBy(['level', 'tnum'])
+    .then(data => {
+      data.map(function(obj) {
+        if (obj.level === 'LEVEL 1') {
+          level_data.level1.push(obj.num)
+        } else if (obj.level === 'LEVEL 2') {
+          level_data.level2.push(obj.num)
+        } else if (obj.level === 'LEVEL 3') {
+          level_data.level3.push(obj.num)
+        } else if (obj.level === 'LEVEL 4') {
+          level_data.level4.push(obj.num)
+        }
+      })
+      res.json(level_data)
+    });
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("server is running on port 3001");
+});
